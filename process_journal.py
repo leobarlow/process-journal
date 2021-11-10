1f715c2a214fbd6c5d8d072819be4a3060940b3f
notefive <leo@sbarlow.com>
2021-11-10 03:23:13 +0000

    Add entry length plotting

--- process_journal.py
+++ process_journal.py
@@ -2,11 +2,12 @@
 
 import json
 import os
-import datetime
+from datetime import datetime
 import re
 import sys
 import matplotlib.pyplot as plt
-
+import numpy as np
+import pprint
 
 def parse_entries(filePath):
 	entryDict = {}
@@ -16,7 +17,8 @@ def parse_entries(filePath):
 			with open(os.path.join(filePath, file), 'r') as f:
 				entryPointDictList = json.load(f)['listContent']
 				entryPointList = [point['text'] for point in entryPointDictList]
-				entryDict[os.path.splitext(file)[0]] = entryPointList
+				date = datetime.strptime(os.path.splitext(file)[0], '%d_%m_%y')
+				entryDict[date] = entryPointList
 	return entryDict
 
 def get_text(entryDict):
@@ -25,24 +27,51 @@ def get_text(entryDict):
 	text = '\n'.join(entryList)
 	return text
 
-def match_instruments(instrumentSet, text):
-	instrumentMatchList = list(filter(lambda v: re.match('\w', text), instrumentSet))
-	return instrumentMatchList
+def save_figure(figureName):
+	plt.savefig('{}.png'.format(figureName), dpi=200, bbox_inches='tight', pad_inches=0.2)
+
+def get_entry_lengths(entryDict):
+	entryLengths = []
+	sortedDates = sorted(list(entryDict.keys()))
+	for date in sortedDates:
+		entryText = '\n'.join(entryDict[date])
+		entryLengths.append(len(entryText.strip()))
+	return sortedDates, entryLengths
+
+def plot_entry_lengths(sortedDates, entryLengths):
+    	plt.plot(sortedDates, entryLengths)
+	plt.xticks(rotation='vertical')
+	plt.xlabel("Entry date")
+	plt.ylabel("Entry length (characters)")
+	return plt.plot
 
 def get_instrument_frequencies(instrumentSet, entryDict):
-	for entry in entryDict.keys():
-		entryText = '\n'.join(entry)
-
+	instrumentFrequencies = {}
+	for date in entryDict.keys():
+		entryInstrumentFrequencies = {}
+		entryText = '\n'.join(entryDict[date])
+		for instrument in instrumentSet:
+			entryInstrumentFrequency = sum(1 for _ in re.finditer(r'\b%s\b' % re.escape(instrument), entryText))
+			entryInstrumentFrequencies[instrument] = entryInstrumentFrequency
+		instrumentFrequencies[date] = (entryInstrumentFrequencies)
 	return instrumentFrequencies
 
 def plot_instrument_frequencies(instrumentFrequencies):
+	#sortedDates = sorted(instrumentFrequencies.values())
+	#instrumentFrequencyList = []
+	#for date in sortedDates:
+	#	instrumentFrequencyList.append(np.array(instrumentFrequencies[date]))
+	#instrumentFrequencyArray = np.array(instrumentFrequencyList)
+	#print(instrumentFrequencyArray)
+	
+	#plt.bar(range(len(instrumentFrequencies)), list(instrumentFrequencies.values()), align='center')
+	#plt.xticks(range(len(instrumentFrequencies)), list(instrumentFrequencies.keys()))
 	return plt.plot
 
 if __name__ == "__main__":
-	filePath = os.path.abspath('takeout/Keep/')
+	filePath = os.path.abspath('takeout-20211110T024606Z-001/Keep/')
 	
 	entryDict = parse_entries(filePath)
-	sys.exit(0)
 
 	instrumentSet = {'piano',
 			 'accordion',
@@ -55,6 +84,12 @@ if __name__ == "__main__":
 			 'banjo',
 			 'tin whistle',
 			 'harmonica'}
+
+
+	sortedDates, entryLengths = get_entry_lengths(entryDict)
+	plot_entry_lengths(sortedDates, entryLengths)
+	save_figure('entry_lengths')
+	sys.exit(0)
 
 	instrumentFrequencies = get_instrument_frequencies(instrumentSet, entryDict)
 	plot_instrument_frequencies(instrumentFrequencies)
